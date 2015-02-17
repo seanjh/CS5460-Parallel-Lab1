378678a76d23e637c69a24df1b8fffd41b63ef61
Sean Herman <seanherman@gmail.com>
2015-02-16 22:52:42 -0500

    message passing. commented out dot product for now.

--- mpi-profile.c
+++ mpi-profile.c
@@ -7,8 +7,9 @@
 
 const int DIM = 10000;
 const int MAX_INT = 10000;
-const int ITER = 10;
+const int ITER = 100;
 const int REPS = 10;
+const int BUFF_SIZE = 1000000000;
 
 int getRandomInt (int max)
 {
@@ -56,15 +57,78 @@ void calcRandomDotProducts (int reps)
     );
 }
 
+void clock_buffer_transfer(char * send_buff, char * recv_buff, int buff_size,
+ int world_rank, int send_partner, int recv_partner)
+{
+  double starttime, endtime, recvtime;
+  printf("Sending %0.4f MB (%d bytes) #%d --> #%d\n", 
+    (double)buff_size / 1000000, buff_size, world_rank, send_partner);
+
+  starttime = MPI_Wtime();
+
+  if (world_rank != 0) {
+
+    MPI_Send(send_buff, buff_size, MPI_CHAR, send_partner, 
+      1, MPI_COMM_WORLD);
+    recvtime = MPI_Wtime();
+    MPI_Recv(recv_buff, buff_size, MPI_CHAR, recv_partner, 
+      1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+  } else {
+
+    recvtime = MPI_Wtime();
+    MPI_Recv(recv_buff, buff_size, MPI_CHAR, recv_partner, 
+      1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Send(send_buff, buff_size, MPI_CHAR, send_partner, 
+      1, MPI_COMM_WORLD);
+
+  }
+
+  endtime = MPI_Wtime();
+
+  // data transfer time = latency + message size / bandwidth
+  printf("Received #%d <-- #%d in %0.4f ms \n", 
+      world_rank, send_partner, (endtime-starttime) * 1000);
+}
+
+void pass_buffer(int world_size, int world_rank, int buff_size)
+{
+  // Pair each process with a sender and recipient
+  int send_partner, recv_partner;
+  send_partner = (world_rank + 1) % world_size;
+  recv_partner = (world_rank == 0) ? world_size - 1 : world_rank - 1;
+
+  // Allocate the buffers
+  char *send_buff = (char *) malloc(buff_size * sizeof(char));
+  char *recv_buff = (char *) malloc(buff_size * sizeof(char));
+
+  int scale[4] = {
+    0, 1000, 1000000, 1000000000
+  };
+  int i;
+  for (i=0; i<=4; i++) {
+    clock_buffer_transfer(
+      send_buff, recv_buff, scale[i],
+      world_rank, send_partner, recv_partner
+    );
+  }
+
+  free(send_buff);
+  free(recv_buff);
+}
+
 int main (int argc, char **argv)
 {
-  // Message latency
-  // Message bandwidth
-  // Computation time per operation (compute a large dot product, and divide by the number of floating point operations)
-  int i;
-  for (i=0; i<ITER; i++) {
-    calcRandomDotProducts(REPS);
-  }
+  
+  int world_size, world_rank;
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  pass_buffer(world_size, world_rank, BUFF_SIZE);
+
+  MPI_Finalize();
 
   return EXIT_SUCCESS;
 }
