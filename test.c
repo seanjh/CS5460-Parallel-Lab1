9ef7ad2d890399d1a283ab1dcac05ffb72d40e89
James <james.kizer@gmail.com>
2015-02-18 16:58:53 -0500

    working distributed dot product

--- test.c
+++ test.c
@@ -9,11 +9,54 @@
 // MPI_Recv(recv_buff, buff_size, MPI_CHAR, MPI_ANY_SOURCE, 
 //       MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+// int MPI_Send(const void* buf, int count, MPI_Datatype datatype, int dest,
+//                   int tag, MPI_Comm comm)
+
+
 // typedef struct DotProductDescriptionStruct {
 //    int a_len;
 //    int b_len;
 //    double vectorBuffer[];
 // } DotProductDescription;
+
+double dotProduct(double *a, double *b, int len)
+{
+  int i;
+  double result = 0.0;
+  for (i=0; i<len; i++)
+    result += a[i]*b[i];
+  return result;
+}
+
+
+void workerTask(int id, int maxLen)
+{
+  int i;
+  double *a; 
+  int a_len;
+  MPI_Status a_status;
+  double *b;
+  int b_len;
+  MPI_Status b_status;
+
+  double partialResult=0.0;
+
+  a = (double *)malloc(maxLen * sizeof(double));
+  b = (double *)malloc(maxLen * sizeof(double));
+  MPI_Recv(a, maxLen, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &a_status);
+  MPI_Recv(b, maxLen, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &b_status);
+
+  MPI_Get_count(&a_status, MPI_DOUBLE, &a_len);
+  MPI_Get_count(&b_status, MPI_DOUBLE, &b_len);
+
+  assert(a_len == b_len);
+
+  partialResult=dotProduct(a, b, a_len);
+
+  //printf ("Hello, I am %d. My partial result is %f.\n", id, partialResult);
+
+  MPI_Send(&partialResult, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+}
 
 #define BUFF_SIZE 1024
 int main (int argc, char **argv)
@@ -21,9 +64,6 @@ int main (int argc, char **argv)
   
   int sz, myid, ai;
   int testLen;
-
-  //double recv_A[BUFF_SIZE];
-  //double recv_B[BUFF_SIZE];
 
   MPI_Init(&argc, &argv);
 
@@ -34,7 +74,7 @@ int main (int argc, char **argv)
   {
     if(myid==0)
       fprintf(stderr, "Usage ./test length\n");
-    
+
     exit(1);
   }
 
@@ -42,25 +82,17 @@ int main (int argc, char **argv)
   //printf("testLen=%d\n", testLen);
 
   
+  //compute parallel results
 
-  // if(myid==0)
-  // {
-  //   printf("argc=%d\n", argc);
-  //   for(ai=0; ai<argc; ai++)
-  //     printf("%s\n", argv[ai]);
+  //first, generate test vectors
 
-  // }
-
+  //next, distribute
   if (myid == 0)
   {
 
     int i;
-    //double a[BUFF_SIZE];
     double *a = (double *)malloc(testLen * sizeof(double));
-    //double b[BUFF_SIZE];
     double *b = (double *)malloc(testLen * sizeof(double));
-
-    double result0, result1;
 
     srand((unsigned)time(NULL));
 
@@ -70,54 +102,61 @@ int main (int argc, char **argv)
       b[i]=((double)rand()/(double)RAND_MAX);
     }
 
-    MPI_Send(a, testLen, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD);
-    MPI_Send(b, testLen, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD);
+    double result;
 
-    MPI_Recv(&result1, 1, MPI_DOUBLE, 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    double starttime, endtime;
 
-    printf ("Result computed by 1: %f\n", result1);
+    starttime = MPI_Wtime();
+    result=dotProduct(a, b, testLen);
+    endtime = MPI_Wtime();
+    printf("Single result: %f\n", result);
+    printf("Computed in %0.4f ms \n", (endtime-starttime) * 1000);
 
-    result0=0.0;
-    for(i=0; i<testLen; i++)
-      result0 += a[i]*b[i];
+    //distribute
+    starttime = MPI_Wtime();
 
-    printf ("Result computed by 0: %f\n", result0);
+    assert(testLen % sz == 0);
+    int partitionLen = testLen/sz;
+    for(i=1; i<sz; i++)
+    {
+      double *a_src = a + (i*partitionLen);
+      double *b_src = b + (i*partitionLen);
 
+      //printf("Sending data to %d\n", i);
+      MPI_Send(a_src, partitionLen, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+      //printf("Sending data to %d\n", i);
+      MPI_Send(b_src, partitionLen, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+    }
 
-    // DotProductDescription desc;
-    // desc.a_len = 16;
-    // desc.b_len = 1024;
-    // memcpy(desc.vectorBuffer, a, 16);
-    // memcpy(desc.vectorBuffer, a, 16);
-  } else if (myid == 1)
+    //compute partial result for master
+    double partialResult;
+
+    partialResult=dotProduct(a, b, partitionLen);
+
+    //printf ("Hello, I am %d. My partial result is %f.\n", myid, partialResult);
+
+    //combine results
+    result=partialResult;
+    for(i=1; i<sz; i++)
+    {
+      MPI_Recv(&partialResult, 1, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      result += partialResult;
+    }
+
+    endtime = MPI_Wtime();
+
+    printf ("Distributed results: %f\n", result);
+    printf("Computed in %0.4f ms \n", (endtime-starttime) * 1000);
+
+  } 
+
+  //receive data and compute partial results
+  else 
   {
-    int i;
-    //double a[BUFF_SIZE];
-    double *a = (double *)malloc(testLen * sizeof(double));
-    int a_len;
-    MPI_Status a_status;
-    //double b[BUFF_SIZE];
-    double *b = (double *)malloc(testLen * sizeof(double));
-    int b_len;
-    MPI_Status b_status;
-
-    double result=0.0;
-
-    MPI_Recv(a, testLen, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &a_status);
-    MPI_Recv(b, testLen, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &b_status);
-
-    MPI_Get_count(&a_status, MPI_DOUBLE, &a_len);
-    MPI_Get_count(&b_status, MPI_DOUBLE, &b_len);
-
-    assert(a_len == b_len);
-
-    for(i=0; i<a_len; i++)
-      result += a[i]*b[i];
-
-    MPI_Send(&result, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+    assert(testLen % sz == 0);
+    int partitionLen = testLen/sz;
+    workerTask(myid, partitionLen);
   }
-
-  //printf ("Hello, I am %d of %d processors!\n", myid, sz);
 
   MPI_Finalize();
 
