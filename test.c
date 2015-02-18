792e060cdc1603c9b405c139c9a3ed740bb7a04e
James <james.kizer@gmail.com>
2015-02-18 16:13:20 -0500

    Added command line argument for length of vectors

--- test.c
+++ test.c
@@ -15,45 +15,70 @@
 //    double vectorBuffer[];
 // } DotProductDescription;
 
-#define BUFF_SIZE 1024*16
+#define BUFF_SIZE 1024
 int main (int argc, char **argv)
 {
   
-  int sz, myid;
+  int sz, myid, ai;
+  int testLen;
 
-  double recv_A[BUFF_SIZE];
-  double recv_B[BUFF_SIZE];
+  //double recv_A[BUFF_SIZE];
+  //double recv_B[BUFF_SIZE];
 
   MPI_Init(&argc, &argv);
 
   MPI_Comm_size (MPI_COMM_WORLD, &sz);
   MPI_Comm_rank (MPI_COMM_WORLD, &myid);
 
+  if (argc != 2)
+  {
+    if(myid==0)
+      fprintf(stderr, "Usage ./test length\n");
+    
+    exit(1);
+  }
+
+  testLen = atoi(argv[1]);
+  //printf("testLen=%d\n", testLen);
+
+  
+
+  // if(myid==0)
+  // {
+  //   printf("argc=%d\n", argc);
+  //   for(ai=0; ai<argc; ai++)
+  //     printf("%s\n", argv[ai]);
+
+  // }
+
   if (myid == 0)
   {
 
     int i;
-    double a[BUFF_SIZE];
-    double b[BUFF_SIZE];
+    //double a[BUFF_SIZE];
+    double *a = (double *)malloc(testLen * sizeof(double));
+    //double b[BUFF_SIZE];
+    double *b = (double *)malloc(testLen * sizeof(double));
+
     double result0, result1;
 
     srand((unsigned)time(NULL));
 
-    for(i=0; i<BUFF_SIZE; i++)
+    for(i=0; i<testLen; i++)
     {
       a[i]=((double)rand()/(double)RAND_MAX);
       b[i]=((double)rand()/(double)RAND_MAX);
     }
 
-    MPI_Send(a, BUFF_SIZE, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD);
-    MPI_Send(b, BUFF_SIZE, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD);
+    MPI_Send(a, testLen, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD);
+    MPI_Send(b, testLen, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD);
 
     MPI_Recv(&result1, 1, MPI_DOUBLE, 1, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     printf ("Result computed by 1: %f\n", result1);
 
     result0=0.0;
-    for(i=0; i<BUFF_SIZE; i++)
+    for(i=0; i<testLen; i++)
       result0 += a[i]*b[i];
 
     printf ("Result computed by 0: %f\n", result0);
@@ -67,17 +92,19 @@ int main (int argc, char **argv)
   } else if (myid == 1)
   {
     int i;
-    double a[BUFF_SIZE];
+    //double a[BUFF_SIZE];
+    double *a = (double *)malloc(testLen * sizeof(double));
     int a_len;
     MPI_Status a_status;
-    double b[BUFF_SIZE];
+    //double b[BUFF_SIZE];
+    double *b = (double *)malloc(testLen * sizeof(double));
     int b_len;
     MPI_Status b_status;
 
     double result=0.0;
 
-    MPI_Recv(a, BUFF_SIZE, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &a_status);
-    MPI_Recv(b, BUFF_SIZE, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &b_status);
+    MPI_Recv(a, testLen, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &a_status);
+    MPI_Recv(b, testLen, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &b_status);
 
     MPI_Get_count(&a_status, MPI_DOUBLE, &a_len);
     MPI_Get_count(&b_status, MPI_DOUBLE, &b_len);
@@ -90,7 +117,7 @@ int main (int argc, char **argv)
     MPI_Send(&result, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
   }
 
-  printf ("Hello, I am %d of %d processors!\n", myid, sz);
+  //printf ("Hello, I am %d of %d processors!\n", myid, sz);
 
   MPI_Finalize();
 
