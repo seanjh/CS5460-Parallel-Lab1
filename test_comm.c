ca20fd881ba86b69c306317ce8f3bd3a3ff92603
Sean Herman <seanherman@gmail.com>
2015-02-20 00:42:20 -0500

    fixed hang on MPI_Finalize

--- test_comm.c
+++ test_comm.c
@@ -2,21 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
 #include <mpi.h>
-
-void pair_processes(int id, int *partner, bool *is_sender, int world_size)
-{
-  // Pair each process with its neighbor
-  assert(world_size % 2 == 0);
-  if (id % 2 == 0) {
-    *is_sender = true;
-    *partner = id + 1;
-  } else {
-    *is_sender = false;
-    *partner = id - 1;
-  }
-  // printf("Process #%d is paired with process #%d\n", id, *partner);
-}
 
 void parse_cli_args(int argc, char **argv, int myid, int *iter)
 {
@@ -32,46 +19,63 @@ void parse_cli_args(int argc, char **argv, int myid, int *iter)
 
 int main (int argc, char **argv)
 {
-  int sz, myid;
+  int sz, myid, tag;
 
   MPI_Init(&argc, &argv);
 
   MPI_Comm_size (MPI_COMM_WORLD, &sz);
   MPI_Comm_rank (MPI_COMM_WORLD, &myid);
+  tag = 1;
 
   int testIterations;
   parse_cli_args(argc, argv, myid, &testIterations);
 
   int partner;
   bool is_sender;
-  pair_processes(myid, &partner, &is_sender, sz);
+
+  // Pair each process with its neighbor
+  assert(sz % 2 == 0);
+  if (myid % 2 == 0) {
+    partner = myid + 1;
+    is_sender = true;
+  } else {
+    partner = myid - 1;
+    is_sender = false;
+  }
+
+  // if (is_sender) {
+  //   printf("Process #%d is a sender\n", myid);
+  // }
 
   double starttime, endtime, total_time;
   MPI_Status status;
-  int count = testIterations;
-  char val = 'a';
+  
+  char inVal, outVal = 'a';
+  int count;
 
-  MPI_Barrier(MPI_COMM_WORLD);
-  while (count > 0) {
+  
+  for (count = testIterations; count > 0; count--) {
     if (is_sender) {
-      count--;
-      // printf("Process #%d sending %d\n", myid, count);
       starttime = MPI_Wtime();
-      MPI_Send(&val, 1, MPI_CHAR, partner, 1, MPI_COMM_WORLD);
-      MPI_Recv(&val, 1, MPI_CHAR, partner, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      MPI_Send(&outVal, 1, MPI_CHAR, partner, tag, MPI_COMM_WORLD);
+      MPI_Recv(&inVal, 1, MPI_CHAR, partner, tag, MPI_COMM_WORLD, &status);
       endtime = MPI_Wtime();
       total_time += (endtime-starttime);
     } else {
-      MPI_Recv(&val, 1, MPI_CHAR, partner, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-      MPI_Send(&val, 1, MPI_CHAR, partner, 1, MPI_COMM_WORLD);
+      MPI_Recv(&inVal, 1, MPI_CHAR, partner, tag, MPI_COMM_WORLD, &status);
+      MPI_Send(&outVal, 1, MPI_CHAR, partner, tag, MPI_COMM_WORLD);
     }
   }
 
+  MPI_Barrier(MPI_COMM_WORLD);
+  
   if (is_sender) {
     double average_latency = total_time / testIterations / 2;
     printf("Average latency between process %d and %d after %d messages is %0.6f µs\n", 
       myid, partner, testIterations, average_latency * 1000000);
   }
-  
+
   MPI_Finalize();
+
+  return EXIT_SUCCESS;
 }
